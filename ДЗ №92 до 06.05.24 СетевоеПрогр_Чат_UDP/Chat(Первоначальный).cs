de562be43cb45b6f4afdae2e5755a4c489582eb1
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-05-12 23:41:18 +0500

    Add files via upload

---
+++ ДЗ №92 до 06.05.24 СетевоеПрогр_Чат_UDP/Chat(Первоначальный).cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace L7_Chat
+{
+    internal class Program
+    {
+        static int RemotePort;
+        static int LocalPort;
+        static IPAddress RemoteIP;       
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.SetWindowSize(40, 20);
+                Console.Title = "Chat";
+                Console.Write("Введите удалённый IP: ");
+                RemoteIP = IPAddress.Parse(Console.ReadLine());
+                Console.Write("Введите удалённый порт: ");
+                RemotePort = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите локальный порт: ");
+                LocalPort = Convert.ToInt32(Console.ReadLine());
+                Thread thread = new Thread(new ThreadStart(ReceiveThread));
+                thread.IsBackground = true;
+                thread.Start();
+                Console.ForegroundColor = ConsoleColor.Red;
+                while (true)
+                {
+                    SendData(Console.ReadLine());
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Преобразование невозможно: " + ex.Message);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ReceiveThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    UdpClient uClient = new UdpClient(LocalPort);
+                    IPEndPoint ipEnd = null;
+                    byte[] response = uClient.Receive(ref ipEnd);
+                    string res = Encoding.Unicode.GetString(response);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(res);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    uClient.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void SendData(string datagram)
+        {
+            UdpClient uClient = new UdpClient();
+            IPEndPoint ipEnd = new IPEndPoint(RemoteIP, RemotePort);
+            try
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(datagram);
+                uClient.Send(bytes, bytes.Length, ipEnd);
+            }
+
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                uClient.Close();
+            }
+        }
+    }
+}
+
+
---
+++ ДЗ №92 до 06.05.24 СетевоеПрогр_Чат_UDP/Chat(конечный).cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace L7_Chat
+{
+    internal class Program
+    {
+        static int RemotePort;
+        static int LocalPort;
+        static IPAddress RemoteIP;
+        static string UserName;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.SetWindowSize(40, 20);
+                Console.Title = "Chat";
+                Console.Write("Введите имя пользователя: ");
+                UserName = Console.ReadLine();
+                Console.Write("Введите удалённый IP: ");
+                RemoteIP = IPAddress.Parse(Console.ReadLine());
+                Console.Write("Введите удалённый порт: ");
+                RemotePort = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите локальный порт: ");
+                LocalPort = Convert.ToInt32(Console.ReadLine());
+                Thread thread = new Thread(new ThreadStart(ReceiveThread));
+                thread.IsBackground = true;
+                thread.Start();
+                Console.ForegroundColor = ConsoleColor.Red;
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    string formattedMessage = $"{UserName}: {message}";
+                    SendData(formattedMessage);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Преобразование невозможно: " + ex.Message);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ReceiveThread()
+        {
+            try
+            {
+                UdpClient uClient = new UdpClient(LocalPort);
+                IPEndPoint ipEnd = null;
+                while (true)
+                {
+                    byte[] response = uClient.Receive(ref ipEnd);
+                    string res = Encoding.Unicode.GetString(response);
+                    int colonIndex = res.IndexOf(':');
+                    if (colonIndex != -1)
+                    {
+                        string senderName = res.Substring(0, colonIndex + 1);
+                        string messageText = res.Substring(colonIndex + 1);
+
+                        Console.ForegroundColor = ConsoleColor.Magenta; // Цвет имени пользователя
+                        Console.Write(senderName + " ");
+                        Console.ForegroundColor = ConsoleColor.Green; // Цвет сообщения
+                        Console.WriteLine(messageText);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Red; // Возврат цвета для ввода
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void SendData(string datagram)
+        {
+            UdpClient uClient = new UdpClient();
+            IPEndPoint ipEnd = new IPEndPoint(RemoteIP, RemotePort);
+            try
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(datagram);
+                uClient.Send(bytes, bytes.Length, ipEnd);
+            }
+
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Ошибка соединения: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                uClient.Close();
+            }
+        }
+    }
+}
+
