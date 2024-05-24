58064f9df9197c10547dc903c5e9e90262bd4ca8
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-05-24 21:18:50 +0500

    Add files via upload

---
+++ ДЗ №97 до 21.05.24 СетевоеПрогр_Parcing/HtmlLibrary/HtmlFetcher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HtmlLibrary
+{
+    public static class HtmlFetcher
+    {
+        public static async Task<string> GetHtmlAsync(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return await client.DownloadStringTaskAsync(new Uri(url));
+            }
+        }
+    }
+}
